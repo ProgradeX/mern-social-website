9de1fe8806602d9db5095737285cbc0e4a894177
ProgradeX <blockybro02@gmail.com>
2023-08-08 17:40:10 +0500

    added auth

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth.js";
 
 // CONFIGURATIONS
 
@@ -36,6 +38,11 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage })
+
+// ROUTES WITH FILES
+
+app.post("/auth/register", upload.single("picture"), register);
+
 
 // MONGOOSE
 
